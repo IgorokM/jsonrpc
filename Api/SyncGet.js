273d1597b4884225558feb833f6db824ab421152
IgorM <mamonenko@aliasworlds.com>
2020-10-07 12:31:46 +0000

    refactoring

--- Api/SyncGet.js
+++ Api/SyncGet.js
@@ -6,7 +6,6 @@ module.exports = async function SyncGet({ token }) {
         throw new RpcException(RpcExceptionMap.InvalidParams);
     }
     const sql = 'SELECT `player`, `data`, UNIX_TIMESTAMP(`time`) AS `time` FROM `sync` WHERE `player`=?';
-    const [rows, fields] = await pool.execute(sql, [320462]);
-    console.log(rows);
+    const [rows] = await pool.execute(sql, [320462]);
     return rows[0];
 };
